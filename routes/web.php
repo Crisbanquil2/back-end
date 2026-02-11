8c9c0020dbabd817a2e0e07af84c50b1f35370f9
Crisbanquil2 <c.banquil.143278.tc@umindanao.edu.ph>
2026-02-11 08:51:19 +0800

    back_end
    Assignment

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('/seed-post', function () {
+    $categoryId = \Illuminate\Support\Facades\DB::table('categories')->insertGetId([
+        'name' => 'Sample Category',
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]);
+
+    \App\Models\Post::create([
+        'title' => 'Sample Post',
+        'text' => 'This is a sample content.',
+        'category_id' => $categoryId,
+    ]);
+
+    return 'OK';
 });
